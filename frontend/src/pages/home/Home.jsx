97fa896606717f650d53ff0babf4c103a9bd61d1
samKit88 <samuel.girma.cx9896@gmail.com>
2023-09-13 10:17:51 +0300

    assets

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -5,12 +5,18 @@ import Banner from '../../components/Banner'
 
 //pages
 import BestSellerBooks from './BestSellerBooks'
+import FavBook from './FavBook'
+import PromoBanner from './PromoBanner'
+// import Review from './Review'
 
 const Home = () => {
   return (
     <div className=" bg-gray-100">
       <Banner />
-      {/* <BestSellerBooks /> */}
+      <BestSellerBooks />
+      <FavBook />
+      <PromoBanner />
+      {/* <Review /> */}
       <div className=" h-screen"></div>
     </div>
   )
