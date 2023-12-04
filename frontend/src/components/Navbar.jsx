73424c467aa4f14703efdfe8da24b0384ec3ac19
samKit88 <samuel.girma.cx9896@gmail.com>
2023-12-04 11:00:36 +0300

    Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,34 +1,76 @@
-import { Link } from "react-router-dom";
-import { useLogout } from "../hook/useLogout";
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { FaBars } from 'react-icons/fa6'
 
-export const Navbar = () => {
-  const { logout } = useLogout();
+const Navbar = () => {
+  const [isScrolly, setIsScrolly] = useState(false)
 
-  const handleClick = () => {
-    logout();
-  };
+  const nav = [
+    {
+      link: 'Home',
+      path: '/',
+    },
+    {
+      link: 'About',
+      path: '/about',
+    },
+    {
+      link: 'Blogs',
+      path: '/blogs',
+    },
+    {
+      link: 'Shop',
+      path: '/shop',
+    },
+  ]
+
+  useEffect(() => {
+    const handleScrolly = () => {
+      if (window.scrollY > 100) {
+        setIsScrolly(true)
+      } else {
+        setIsScrolly(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScrolly)
+    return () => window.removeEventListener('scroll', handleScrolly)
+  }, [])
+
   return (
-    
+    <header className="">
+      <nav>
+        <div
+          className={`${
+            isScrolly
+              ? '  bg-red-500 h-12 fixed top-0 left-0 right-0  flex justify-between items-center  '
+              : ''
+          }`}
+        >
+          <div className="text-center md:mt-16 md:mb-16 mt-8 mb-8">
+            {/* btn for lg device */}
+            <button className=" md:hidden mr-18">
+              <FaBars />
+            </button>
+            <Link to="/" className=" md:text-6xl text-3xl">
+              Book Store
+            </Link>
+          </div>
+          <hr />
+          {/* nav items for large device */}
+          <ul className="md:flex md:justify-center uppercase space-x-52 hidden ">
+            {nav.map(({ link, path }) => (
+              <Link to={path} key={path}>
+                {link}
+              </Link>
+            ))}
+          </ul>
 
-    <header>
-      <div className="container">
-        <Link to="/">
-          <h1>BookWise</h1>
-        </Link>
-        <nav>
-          {
-<div>
-            <button onClick={handleClick}>Logout</button>
-          </div>
-          }
-          
-          <div>
-                 <Link to="/login"> Login </Link>
-            <Link to="/signup"> Signup</Link>
-           
-          </div>
-        </nav>
-      </div>
+          <hr />
+        </div>
+      </nav>
     </header>
-  );
-};
+  )
+}
+
+export default Navbar
