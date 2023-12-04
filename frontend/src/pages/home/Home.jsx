0dfd388eb90358201a7f420528c51b29d913a154
samKit88 <samuel.girma.cx9896@gmail.com>
2023-12-04 10:56:31 +0300

    tailwindcss

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className=" h-screen ">Home</div>
+      <div className=" h-screen bg-slate-700"></div>
+    </div>
+  )
+}
+
+export default Home
