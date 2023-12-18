d7c542d60df092a7bc65b9cd7a3ad0acdb1c0fdc
samKit88 <samuel.girma.cx9896@gmail.com>
2023-12-18 13:23:43 +0300

    all books

---
+++ frontend/src/components/AllBooks.jsx
@@ -0,0 +1,201 @@
+import React from 'react'
+
+import book1 from '../assets/BookImages/Book3.png'
+
+const AllBooks = () => {
+  return (
+    <dev className="grid md:grid-cols-3  gap-4 md:px-24 pl-24">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+        <img src={book1} alt="" className="w-full" />
+        <div className="px-6 py4">
+          <div className="font-bold text-purple-500 text-xl">image.user</div>
+          <ul>
+            <li>
+              <strong>Likes:</strong>
+              image.likes
+            </li>
+            <li>
+              <strong>Downloads:</strong>
+              image.downloads
+            </li>
+            <li>
+              <strong>Views:</strong>
+              image.views
+            </li>
+          </ul>
+        </div>
+        <div className="px-6 py-4">Tag tags=tags</div>
+      </div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+        <img src={book1} alt="" className="w-full" />
+        <div className="px-6 py4">
+          <div className="font-bold text-purple-500 text-xl">image.user</div>
+          <ul>
+            <li>
+              <strong>Likes:</strong>
+              image.likes
+            </li>
+            <li>
+              <strong>Downloads:</strong>
+              image.downloads
+            </li>
+            <li>
+              <strong>Views:</strong>
+              image.views
+            </li>
+          </ul>
+        </div>
+        <div className="px-6 py-4">Tag tags=tags</div>
+      </div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+        <img src={book1} alt="" className="w-full" />
+        <div className="px-6 py4">
+          <div className="font-bold text-purple-500 text-xl">image.user</div>
+          <ul>
+            <li>
+              <strong>Likes:</strong>
+              image.likes
+            </li>
+            <li>
+              <strong>Downloads:</strong>
+              image.downloads
+            </li>
+            <li>
+              <strong>Views:</strong>
+              image.views
+            </li>
+          </ul>
+        </div>
+        <div className="px-6 py-4">Tag tags=tags</div>
+      </div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+        <img src={book1} alt="" className="w-full" />
+        <div className="px-6 py4">
+          <div className="font-bold text-purple-500 text-xl">image.user</div>
+          <ul>
+            <li>
+              <strong>Likes:</strong>
+              image.likes
+            </li>
+            <li>
+              <strong>Downloads:</strong>
+              image.downloads
+            </li>
+            <li>
+              <strong>Views:</strong>
+              image.views
+            </li>
+          </ul>
+        </div>
+        <div className="px-6 py-4">Tag tags=tags</div>
+      </div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+        <img src={book1} alt="" className="w-full" />
+        <div className="px-6 py4">
+          <div className="font-bold text-purple-500 text-xl">image.user</div>
+          <ul>
+            <li>
+              <strong>Likes:</strong>
+              image.likes
+            </li>
+            <li>
+              <strong>Downloads:</strong>
+              image.downloads
+            </li>
+            <li>
+              <strong>Views:</strong>
+              image.views
+            </li>
+          </ul>
+        </div>
+        <div className="px-6 py-4">Tag tags=tags</div>
+      </div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+        <img src={book1} alt="" className="w-full" />
+        <div className="px-6 py4">
+          <div className="font-bold text-purple-500 text-xl">image.user</div>
+          <ul>
+            <li>
+              <strong>Likes:</strong>
+              image.likes
+            </li>
+            <li>
+              <strong>Downloads:</strong>
+              image.downloads
+            </li>
+            <li>
+              <strong>Views:</strong>
+              image.views
+            </li>
+          </ul>
+        </div>
+        <div className="px-6 py-4">Tag tags=tags</div>
+      </div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+        <img src={book1} alt="" className="w-full" />
+        <div className="px-6 py4">
+          <div className="font-bold text-purple-500 text-xl">image.user</div>
+          <ul>
+            <li>
+              <strong>Likes:</strong>
+              image.likes
+            </li>
+            <li>
+              <strong>Downloads:</strong>
+              image.downloads
+            </li>
+            <li>
+              <strong>Views:</strong>
+              image.views
+            </li>
+          </ul>
+        </div>
+        <div className="px-6 py-4">Tag tags=tags</div>
+      </div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+        <img src={book1} alt="" className="w-full" />
+        <div className="px-6 py4">
+          <div className="font-bold text-purple-500 text-xl">image.user</div>
+          <ul>
+            <li>
+              <strong>Likes:</strong>
+              image.likes
+            </li>
+            <li>
+              <strong>Downloads:</strong>
+              image.downloads
+            </li>
+            <li>
+              <strong>Views:</strong>
+              image.views
+            </li>
+          </ul>
+        </div>
+        <div className="px-6 py-4">Tag tags=tags</div>
+      </div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+        <img src={book1} alt="" className="w-full" />
+        <div className="px-6 py4">
+          <div className="font-bold text-purple-500 text-xl">image.user</div>
+          <ul>
+            <li>
+              <strong>Likes:</strong>
+              image.likes
+            </li>
+            <li>
+              <strong>Downloads:</strong>
+              image.downloads
+            </li>
+            <li>
+              <strong>Views:</strong>
+              image.views
+            </li>
+          </ul>
+        </div>
+        <div className="px-6 py-4">Tag tags=tags</div>
+      </div>
+    </dev>
+  )
+}
+
+export default AllBooks
