3829fa4a6b7dd5b36b58f3accbf0423e610e97ac
samKit88 <samuel.girma.cx9896@gmail.com>
2023-12-18 13:23:11 +0300

    flowb

--- frontend/src/pages/home/Shop.jsx
+++ frontend/src/pages/home/Shop.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import SearchImage from '../../components/SearchImage'
+import AllBooks from '../../components/AllBooks'
 
 const Shop = () => {
-  return <div>Shop</div>
+  return (
+    <div className="bg-gray-100">
+      <SearchImage />
+      <AllBooks />
+    </div>
+  )
 }
 
 export default Shop
